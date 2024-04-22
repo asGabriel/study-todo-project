e6dca6c3dea401246a7eb94cdb13bf72d90eee2d
asGabriel <andrade.gabriel2295@gmail.com>
2024-04-21 21:21:40 -0300

    fix: update and remove route

--- src/domains/task.ts
+++ src/domains/task.ts
@@ -1,18 +1,16 @@
 import { UUID } from "crypto";
 
 export interface Task {
-    id: UUID;
-    title: string;
-    isDone: boolean;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at: Date;
+  id: UUID;
+  title: string;
+  isDone: boolean;
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
 }
 
 export interface CreateTaskDto {
-    title: string;
+  title: string;
 }
 
-export interface UpdateTaskDto {
-    title?: string
-}
+export interface UpdateTaskDto extends Partial<CreateTaskDto> {}
