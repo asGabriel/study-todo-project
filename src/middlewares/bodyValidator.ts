136057f6e81de294e9091f90c63b23e2341d72ba
asGabriel <andrade.gabriel2295@gmail.com>
2024-04-25 05:30:02 -0300

    fix(tasks): improvements in middleware validation

--- src/middlewares/bodyValidator.ts
+++ src/middlewares/bodyValidator.ts
@@ -1,13 +1,57 @@
-import express, { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+import { mockCreateTaskDto, mockUpdateTaskDto } from "../domains/task";
 
-export const bodyValidate = (
+const INVALID_PAYLOAD_MESSAGE =
+  "Invalid payload, please check the data structure.";
+const INVALID_PAYLOAD_HTTP_RESPONSE_NUMBER = 400;
+
+export const taskPayloadValidator = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.body || Object.keys(req.body).length === 0) {
-    return res.status(400).send("Invalid body");
+  const httpMethod = req.method;
+  const payload = req.body;
+
+  switch (httpMethod) {
+    case "POST":
+      const isPostPayloadValid = checkPayload(payload, mockCreateTaskDto);
+      if (!isPostPayloadValid) return sendReturnMessage(res);
+      break;
+
+    case "PATCH":
+      const isPatchPayloadValid = checkPayload(payload, mockUpdateTaskDto);
+      if (!isPatchPayloadValid) return sendReturnMessage(res);
+      break;
+
+    case "GET":
+      const isGetPayloadValid = checkPayload(payload, {} as any);
+      if (!isGetPayloadValid) return sendReturnMessage(res);
+      break;
+
+    case "DELETE":
+      const isDeletePayloadValid = checkPayload(payload, {} as any);
+      if (!isDeletePayloadValid) return sendReturnMessage(res);
+      break;
+    default:
+      break;
   }
-  
+
   next();
+};
+
+const checkPayload = (payload: any, structure: any): boolean => {
+  const payloadKey = Object.keys(payload);
+  const structureKeys = Object.keys(structure);
+
+  if (!payloadKey) return false;
+
+  const isValid = payloadKey.every((prop) => structureKeys.includes(prop))
+  return isValid;
+};
+
+const sendReturnMessage = (res: Response) => {
+  return res
+    .status(INVALID_PAYLOAD_HTTP_RESPONSE_NUMBER)
+    .send(INVALID_PAYLOAD_MESSAGE);
 };
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -2,23 +2,25 @@ import { Request, Response, Router } from "express";
 import { TaskRepository } from "../repositories/task";
 import db from "../db";
 import { TaskHandler } from "../handlers/task";
-import { bodyValidate } from "../middlewares/bodyValidator";
+import { taskPayloadValidator } from "../middlewares/bodyValidator";
 
 const taskRouter = Router();
 const taskRepository = new TaskRepository(db);
 const taskHandler = new TaskHandler(taskRepository);
 
+taskRouter.use(taskPayloadValidator)
+
 taskRouter
   .get("/tasks", (req: Request, res: Response) =>
     taskHandler.listTasks(req, res)
   )
-  .post("/tasks", bodyValidate, (req: Request, res: Response) =>
+  .post("/tasks", (req: Request, res: Response) =>
     taskHandler.createTask(req, res)
   )
   .get("/tasks/:id", (req: Request, res: Response) =>
     taskHandler.getTaskById(req, res)
   )
-  .patch("/tasks/:id", bodyValidate, (req: Request, res: Response) =>
+  .patch("/tasks/:id", (req: Request, res: Response) =>
     taskHandler.updateTaskById(req, res)
   )
   .delete("/tasks/:id", (req: Request, res: Response) =>
